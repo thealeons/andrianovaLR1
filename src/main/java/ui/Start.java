1fa66cf3b7f87e4ab317a86f3a30fe19124d1526
aleons <thealeons@gmail.com>
2021-01-08 04:00:15 +0300

    Реализовано UI генерации ключа

--- src/main/java/ui/Start.java
+++ src/main/java/ui/Start.java
@@ -1,20 +1,39 @@
 package ui;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import logic.KeyGenerator;
 
 public class Start {
 
     public Button createKey;
     public Label key;
-    public Button generateKey;
+    public Button randomKey;
+    public DialogPane addKey;
+    public DialogPane generateKey;
+    public GridPane startButtonsPanel;
+    public Button saveGenerateKey;
+    public TextField generateKeyValue;
 
     public void addKey(MouseEvent mouseEvent) {
-        
+        addKey.setVisible(true);
     }
 
     public void generateKey(MouseEvent mouseEvent) {
+        startButtonsPanel.setVisible(false);
+        generateKey.setVisible(true);
+    }
 
+    public void saveGenerateKey(MouseEvent mouseEvent) {
+        try {
+            key.setText(new KeyGenerator().getKey(Integer.parseInt(generateKeyValue.getText())));
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
